ade3a6247cbea6ede179b095a0a6fac4279046e3
Sami Taider <taidersami1@gmail.com>
2025-03-25 14:17:01 +0100

    submission file

--- regression.py
+++ regression.py
@@ -7,9 +7,13 @@ from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import GridSearchCV
 import joblib
+from sklearn.preprocessing import StandardScaler
+
 
 train_df = pd.read_csv("train.csv")
 test_df = pd.read_csv("test.csv")
+test_ids = test_df["id"]  # save the ids for the submission file
+
 pd.set_option("display.max_columns", None)
 
 ### PART 1 : preprocessing of training data :
@@ -19,10 +23,22 @@ pd.set_option("display.max_columns", None)
 
 # drop 'hc' column (too many missing values)
 # train_df = train_df.drop(columns=["id", "hc", "model", "range"])
-# test_df = test_df.drop(columns=["id", "hc", "model", "range"])
 
 # try dropping all non-numerical columns (+ hc)
 train_df = train_df.drop(
+    columns=[
+        "id",
+        "brand",
+        "model",
+        "car_class",
+        "range",
+        "fuel_type",
+        "hybrid",
+        "grbx_type_ratios",
+        "hc",
+    ]
+)
+test_df = test_df.drop(
     columns=[
         "id",
         "brand",
@@ -39,37 +55,26 @@ train_df = train_df.drop(
 # drop rows with missing urb_cons and exturb_cons values
 
 # train_df = train_df.dropna(subset=["urb_cons", "exturb_cons"])
-# test_df = test_df.dropna(subset=["urb_cons", "exturb_cons"])
 
 # try dropping all rows with missing valued
 
 train_df = train_df.dropna(
     subset=["urb_cons", "exturb_cons", "nox", "co", "hcnox", "ptcl"]
 )
-test_df = test_df.dropna(
-    subset=["urb_cons", "exturb_cons", "nox", "co", "hcnox", "ptcl"]
-)
 
 # # for nox, co, hcnox, ptcl, fill missing values with the mean of the column
 
 # train_df["nox"] = train_df["nox"].fillna(train_df["nox"].mean())
-# test_df["nox"] = test_df["nox"].fillna(test_df["nox"].mean())
 
 # train_df["co"] = train_df["co"].fillna(train_df["co"].mean())
-# test_df["co"] = test_df["co"].fillna(test_df["co"].mean())
 
 # train_df["hcnox"] = train_df["hcnox"].fillna(train_df["hcnox"].mean())
-# test_df["hcnox"] = test_df["hcnox"].fillna(test_df["hcnox"].mean())
 
 # train_df["ptcl"] = train_df["ptcl"].fillna(train_df["ptcl"].mean())
-# test_df["ptcl"] = test_df["ptcl"].fillna(test_df["ptcl"].mean())
 
 # # convert brand, car_class to numerical values using one-hot encoding
 # train_df = pd.get_dummies(
 #     train_df, columns=["brand", "car_class", "fuel_type", "grbx_type_ratios"]
-# )
-# test_df = pd.get_dummies(
-#     test_df, columns=["brand", "car_class", "fuel_type", "grbx_type_ratios"]
 # )
 
 # verify what valued the hybrid column can take
@@ -77,7 +82,7 @@ test_df = test_df.dropna(
 
 # # convert hybrid column to binary values
 # train_df["hybrid"] = train_df["hybrid"].map({"non": 0, "oui": 1})
-# test_df["hybrid"] = test_df["hybrid"].map({"non": 0, "oui": 1})
+
 
 # Split data
 X = train_df.drop(columns=["co2"])
@@ -86,6 +91,11 @@ y = train_df["co2"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
+# normalize data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 ### PART 2 : training a regression model
 ## basic model first
@@ -125,7 +135,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 # # Save the model with the best parameters
 # joblib.dump(best_model, "best_model.pkl")
 
-# train a model with the best params
+# # train a model with the best params
 best_model = joblib.load("best_model.pkl")
 best_model.fit(X_train, y_train)
 y_pred = best_model.predict(X_test)
@@ -133,3 +143,10 @@ mae = mean_absolute_error(y_test, y_pred)
 print(f"Mean Absolute Error: {mae}")
 relative_error = (mae / train_df["co2"].mean()) * 100
 print(f"Relative Error: {relative_error:.2f}%")
+
+# # submit file
+
+final_predictions = best_model.predict(test_df)
+submission = pd.DataFrame({"id": test_ids, "co2": final_predictions})
+submission.to_csv("submission.csv", index=False)
+print("File generated : submission.csv")
